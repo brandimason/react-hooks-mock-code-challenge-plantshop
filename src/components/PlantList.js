ab25119eb4a9262bd40a35909f63cde221cc2653
Brandi L Mason <masonbrandi66@gmail.com>
2023-07-11 09:17:01 -0600

    partial

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((response) => response.json())
+    .then(data => {
+      PlantCard(data);
+    });
+  }, []);
   return (
     <ul className="cards">{/* render PlantCards components in here */}</ul>
   );
